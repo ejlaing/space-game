daf9c9cf30747506d014486e427cda172515348c
ejlaing <99044854+ejlaing@users.noreply.github.com>
2023-11-28 14:35:56 -0600

    Tutorial

--- sketch.js
+++ sketch.js
@@ -2,6 +2,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -10,6 +17,8 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+
+/// PRELOAD ///
 
 function preload ()
 {
@@ -23,10 +32,24 @@ function preload ()
     );
 }
 
+/// CREATE ///
+
+var platforms;
+
 function create ()
 {
     this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
+
+/// UPDATE ///
 
 function update () 
 {
